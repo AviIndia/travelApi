2c1adaceb221d2379767a9e9b42b6b0e0782d497
Planet_laptop <avijit.mondal@planetinfra.com>
2026-06-23 17:35:16 +0530

    Upload From office

--- config/constant.php
+++ config/constant.php
@@ -7,4 +7,4 @@ define('UPLOAD_URL', BASE_URL . 'uploads/');
 
 define('UPLOAD_PATH', __DIR__ . '/../uploads/');
 
-define('GEMINI_API_KEY', 'AQ.Ab8RN6J2Q4NBDx-Jm2HNKtPo38N-YYvsUbb-0pL6ddp5uxntOw');
+define('GEMINI_API_KEY', '');
